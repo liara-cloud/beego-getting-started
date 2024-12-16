f2ee98a2b7bddfc8781f92ad7bb883f1dd82f00a
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-12-16 15:29:13 +0330

    add some static files

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	_ "mybeegoapp/routers"
-	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web"
 )
 
 func main() {
-	beego.Run()
+	// Set up static directories
+	web.SetStaticPath("/css", "static/css")
+	web.SetStaticPath("/js", "static/js")
+	web.SetStaticPath("/images", "static/images")
+
+	// Serve the HTML file
+	web.Router("/", &MainController{})
+
+	web.Run() // Start the server
 }
 
+// MainController handles the root route
+type MainController struct {
+	web.Controller
+}
+
+func (c *MainController) Get() {
+	c.TplName = "index.html" // Template file to serve
+}
